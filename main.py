9df41ea38552d44891aeedd639991cc3fac0eb47
Sebastian <thelopezepol@estudiantec.cr>
2021-06-17 16:21:33 -0600

    menu

--- main.py
+++ main.py
@@ -6,12 +6,37 @@ def main():
     consult()
     fillEvaluation()
     fillMetrics()
-    for docClass in CLASSES:
-        print("Clase: ", docClass.name)
-        docClass.printEvaluation()
-        docClass.printMetrics()
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    menu()
     return 0
+
+
+def menu():
+    print("\n***************SELECCIONE UNA OPCION****************")
+    print("*\t1) Mostrar test-set.csv")
+    print("*\t2) Mostrar training-set.csv")
+    print("*\t3) Mostrar clases.csv")
+    print("*\t0) Salir")
+    print("*****************************************************")
+    opt = input("Que accion desea realizar? ")
+    print(opt)
+    if opt == '1':
+        for doc in TEST:
+            print("DocID: ", doc.id)
+            print("Pertenece a la clase: ", doc.classDoc, "\tSe le asigno la clase: ", doc.classDocAssumption)
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    elif opt == '2':
+        for doc in TRAIN:
+            doc.printDoc()
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    elif opt == '3':
+        for docClass in CLASSES:
+            print("Clase: ", docClass.name)
+            docClass.printEvaluation()
+            docClass.printMetrics()
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    else:
+        return
+    menu()
 
 
 main()
